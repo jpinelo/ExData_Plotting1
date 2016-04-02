52610f46b0878b08d2cc58156365df8ad1243823
Joao Pinelo <j.pinelo@mac.com>
2016-04-02 12:30:54 +0300

    working plot

---
+++ plot1.R
@@ -0,0 +1,36 @@
+# LIBRARIES
+library(lubridate)
+
+# DATA IMPORT AND PREPARATION
+data <- read.csv2(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+
+# coerce Date to date object
+data$Date <- dmy(data$Date)
+
+# create df with necessary data only: 2007-02-01 and 2007-02-02
+dateStart <- as.POSIXct("2007-02-01")
+dateEnd <- as.POSIXct("2007-02-02")
+int <- new_interval(dateStart, dateEnd) # interval of interest
+df <- data[data$Date %within% int,]
+
+# coerce other variables to appropirate data types
+df1 <- df
+df1$dateTime <- paste(df$Date, df$Time, sep = " ")
+df1$dateTime <- strptime(df1$dateTime, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
+
+df1$Global_active_power <- as.numeric(df1$Global_active_power)
+df1$Global_reactive_power <- as.numeric(df1$Global_reactive_power)
+df1$Voltage <- as.numeric(df1$Voltage)
+df1$Global_intensity <- as.numeric(df1$Global_intensity)
+df1$Sub_metering_1 <- as.numeric(df1$Sub_metering_1)
+df1$Sub_metering_2 <- as.numeric(df1$Sub_metering_2)
+df1$Sub_metering_3 <- as.numeric(df1$Sub_metering_3)
+
+# PLOTTING
+
+# plot1
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+hist(df1$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
